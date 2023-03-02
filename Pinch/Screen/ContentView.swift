fe8a3101d075f49181fdd4d9471f541bdedd0e02
Jonathan Meriño B <jonathan.merino@ceiba.com.co>
2023-03-02 16:21:54 -0500

    Drag gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -11,8 +11,16 @@ struct ContentView: View {
     // MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
+    
+    func resetImageState() {
+        return withAnimation(.spring()){
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     var body: some View {
@@ -25,32 +33,44 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
-                    // MARK: - 1- TAP GESTURE
+                // MARK: - 1- TAP GESTURE
                     .onTapGesture(count: 2) {
                         if imageScale == 1 {
                             withAnimation(.spring()){
                                 imageScale = 5
                             }
                         }else {
-                            withAnimation(.spring()){
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
+                    .gesture(DragGesture()
+                        .onChanged { value in
+                            withAnimation(.linear(duration: 1)){
+                                imageOffset = value.translation
+                            }
+                            
+                        }
+                        .onEnded { value in
+                            if imageScale <= 1 {
+                                resetImageState()
+                            }
+                        }
+                    )
                 
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear {
-               isAnimating = true
+                isAnimating = true
             }
         }
         .navigationViewStyle(.stack)
     }
 }
-    // MARK: - PREVIEW
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
