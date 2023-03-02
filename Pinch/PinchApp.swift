63bd16455d249b82d745545af5da01e06f3eb30c
Jonathan Meriño B <jonathan.merino@ceiba.com.co>
2023-03-02 14:30:24 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jonathan Eduardo Meriño Bolívar - Ceiba Software on 2/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
