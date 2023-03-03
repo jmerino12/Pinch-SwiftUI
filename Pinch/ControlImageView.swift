8043b6b31d463c7c4220c88c2522bf9fb0b5f5c4
Jonathan Meriño B <jonathan.merino@ceiba.com.co>
2023-03-03 07:59:10 -0500

    Control interface

---
+++ Pinch/ControlImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Jonathan Eduardo Meriño Bolívar - Ceiba Software on 3/03/23.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -61,14 +61,57 @@ struct ContentView: View {
                         }
                     )
                 
-            }.overlay(InfoPanelView(scale: imageScale, offset: imageOffset).padding(.horizontal).padding(.top, 30), alignment: .top)
-            
+            }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear {
                 isAnimating = true
             }
-        }
+            .overlay(InfoPanelView(scale: imageScale, offset: imageOffset).padding(.horizontal).padding(.top, 30), alignment: .top)
+            // MARK: - CONTROLS
+            .overlay(Group {
+                HStack {
+                    //SCALE DOWN
+                    Button {
+                        withAnimation(.spring()) {
+                            if imageScale > 1 {
+                                imageScale -= 1
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                        }
+                    }label: {
+                        ControlImageView(icon: "minus.magnifyingglass")
+                    }
+                    //RESET
+                    Button {
+                        resetImageState()
+                    }label: {
+                        ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                    }
+                    //SCALE UP
+                    Button {
+                        withAnimation(.spring()) {
+                            if imageScale < 5 {
+                                imageScale += 1
+                                
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                }
+                            }
+                        }
+                    }label: {
+                        ControlImageView(icon: "plus.magnifyingglass")
+                    }
+                }//: CONTROLS
+                .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                .background(.ultraThinMaterial)
+                .cornerRadius(12)
+                .opacity(isAnimating ? 1 : 0)
+            }.padding(.bottom, 30)
+                     , alignment: .bottom)
+        }//: NAVIGATION
         .navigationViewStyle(.stack)
     }
 }
